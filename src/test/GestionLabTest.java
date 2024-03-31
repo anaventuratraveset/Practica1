fab000573a13b6270e056712b8a7bdd70d800ac2
Ana Ventura-Traveset <a.venturatraveset@usp.ceu.es>
2024-03-31 22:54:47 +0200

    los UnitTest

--- src/test/GestionLabTest.java
+++ src/test/GestionLabTest.java
@@ -21,11 +21,6 @@ public class GestionLabTest {
         Poblacion miPob = GestionLab.createPoblacion(exp);
         assertEquals(miPob, exp.getPoblacionesList().get(0));
 
-        // no consigo hacer el unitTest de createPoblacion() de GestionLab
-        // porque en el método le pido por pantalla varias variables y para ello se hace por ejemplo int.nextInt()
-        // entonces digamos que en la primera pedida por pantalla me se "come" todo el provided input, a pesar de
-        // haberlo intentado separar por saltos de línea
-
     }
 
 }
